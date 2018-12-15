c1b390fb9963d5ee8568f767d55a2ab3886c1378
Kristopher Wilson <kristopherwilson@gmail.com>
2018-12-15 00:05:21 -0500

    Better background color

--- client/src/component/Layout.js
+++ client/src/component/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 const AppContainer = styled.div`
-  background-color: #f9f9f9;
+  background-color: #fff;
   border-radius: 4px;
   box-sizing: border-box;
   padding: 10px 20px;

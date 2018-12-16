749d13ae8298da0831a30cfbd8bc4eb221156270
Kristopher Wilson <kristopherwilson@gmail.com>
2018-12-16 18:40:15 -0500

    Extracting components + functions for future reuse

--- client/src/Transactions/components/Statistics.js
+++ client/src/Transactions/components/Statistics.js
@@ -1,58 +1,25 @@
 import React from 'react';
-import styled from 'styled-components';
-import styledMap from 'styled-map';
-
-const tileColorMap = styledMap`
-  danger: #e3342f;
-  success: #38c172;
-`;
-
-const StatisticTile = styled.div`
-  background-color: ${tileColorMap};
-  border-radius: 4px;
-  box-sizing: border-box;
-  color: #fff;
-  margin-right: 5px;
-  padding: 10px;
-  text-align: right;
-  width: 320px;
-
-  .value {
-    font-size: 2.5em;
-  }
-`;
-
-const Tile = ({ title, value, ...props }) => (
-  <StatisticTile {...props}>
-    <div className="title">{title}</div>
-    <div className="value">{value}</div>
-  </StatisticTile>
-);
-
-const formatter = new Intl.NumberFormat('en-US', {
-  style: 'currency',
-  currency: 'USD',
-  minimumFractionDigits: 2,
-});
+import { StatisticTile } from 'component/StatisticTile';
+import { currencyFormatter } from 'lib/currencyFormatter';
 
 export default ({ month }) => {
   const cashflow = month.totalIncome - Math.abs(month.totalExpenses);
 
   return (
     <div style={{ alignItems: 'center', display: 'flex' }}>
-      <Tile
+      <StatisticTile
         title="Expenses"
-        value={formatter.format(month.totalExpenses)}
+        value={currencyFormatter(month.totalExpenses)}
         danger
       />
-      <Tile
+      <StatisticTile
         title="Income"
-        value={formatter.format(month.totalIncome)}
+        value={currencyFormatter(month.totalIncome)}
         success
       />
-      <Tile
+      <StatisticTile
         title="Net Cash"
-        value={formatter.format(cashflow)}
+        value={currencyFormatter(cashflow)}
         success={cashflow > 0}
         danger={cashflow <= 0}
       />
---
+++ client/src/component/StatisticTile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import styledMap from 'styled-map';
+
+const tileColorMap = styledMap`
+  danger: #e3342f;
+  success: #38c172;
+`;
+
+const Styles = styled.div`
+  background-color: ${tileColorMap};
+  border-radius: 4px;
+  box-sizing: border-box;
+  color: #fff;
+  margin-right: 5px;
+  padding: 10px;
+  text-align: right;
+  width: 320px;
+
+  .value {
+    font-size: 2.5em;
+  }
+`;
+
+export const StatisticTile = ({ title, value, ...props }) => (
+  <Styles {...props}>
+    <div className="title">{title}</div>
+    <div className="value">{value}</div>
+  </Styles>
+);

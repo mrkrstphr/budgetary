10fd4e6c872898594e8ace25d3bb417c5b545695
Kristopher Wilson <kristopherwilson@gmail.com>
2018-12-21 12:52:37 -0500

    Adds holiday header icons (super important)

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -6,8 +6,45 @@ const Styles = styled.h1`
   color: #555;
 `;
 
+const DEFAULT_ICON = 'usd-circle';
+
+const holidayHeaderIcons = [
+  {
+    start: new Date(`${new Date().getFullYear()}-10-20`),
+    end: new Date(`${new Date().getFullYear()}-10-31`),
+    icon: 'jack-o-lantern',
+  },
+  {
+    start: new Date(`${new Date().getFullYear()}-11-20`),
+    end: new Date(`${new Date().getFullYear()}-11-30`),
+    icon: 'turkey',
+  },
+  {
+    start: new Date(`${new Date().getFullYear()}-12-06`),
+    end: new Date(`${new Date().getFullYear()}-12-29`),
+    icon: 'tree-christmas',
+  },
+  {
+    start: new Date(`${new Date().getFullYear()}-12-30`),
+    end: new Date(`${new Date().getFullYear()}-01-03`),
+    icon: 'glass-cheers',
+  },
+];
+
+function getHeaderIcon() {
+  const now = new Date();
+
+  for (const candidate of holidayHeaderIcons) {
+    if (now >= candidate.start && now <= candidate.end) {
+      return candidate.icon;
+    }
+  }
+
+  return DEFAULT_ICON;
+}
+
 export default () => (
   <Styles>
-    Spend <Icon icon="usd-circle" color="#555" /> Tracking
+    Spend <Icon icon={getHeaderIcon()} color="#555" /> Tracking
   </Styles>
 );
--- client/src/component/Icon.js
+++ client/src/component/Icon.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faUsdCircle } from '@fortawesome/pro-solid-svg-icons';
+import {
+  faGlassCheers,
+  faJackOLantern,
+  faTreeChristmas,
+  faTurkey,
+  faUsdCircle,
+} from '@fortawesome/pro-solid-svg-icons';
 
-library.add(faUsdCircle);
+library.add(
+  faGlassCheers,
+  faJackOLantern,
+  faTreeChristmas,
+  faTurkey,
+  faUsdCircle,
+);
 
 export default ({ ...props }) => <FontAwesomeIcon {...props} />;

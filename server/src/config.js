023841268fb683f4b34d55abfec0eed190167347
Kristopher Wilson <kristopherwilson@gmail.com>
2019-08-20 12:12:49 -0400

    WTF

--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,4 @@
 const environment = process.env.NODE_ENV || 'production';
 
-const configurations = require('../../config.json');
+const configurations = require('../config.json');
 export default configurations[environment];

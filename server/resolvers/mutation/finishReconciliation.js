41828db897bc800a4f29f35d8cb25cf82e0f5ee2
Kristopher Wilson <kristopherwilson@gmail.com>
2020-01-10 11:27:53 -0500

    Re-enable finishReconciliation mutation

--- server/resolvers/mutation/finishReconciliation.js
+++ server/resolvers/mutation/finishReconciliation.js
@@ -1,45 +1,47 @@
-// import { isNil } from 'lodash';
-// import * as yup from 'yup';
+const { isNil } = require('lodash');
+const yup = require('yup');
 
-// function validateFinishReconciliation(context) {
-//   return yup.object().shape({
-//     id: yup
-//       .string()
-//       .required()
-//       .test('validate-exists', 'Reconciliation not found', async function(
-//         value,
-//       ) {
-//         const reconciliation = await context.dbal.reconciliation.fetchById(
-//           value,
-//         );
+function validateFinishReconciliation(context) {
+  return yup.object().shape({
+    id: yup
+      .string()
+      .required()
+      .test(
+        'validate-exists',
+        'Reconciliation not found',
+        async function validateReconcilationExists(value) {
+          const reconciliation = await context.dbal.reconciliation.fetchById(
+            value,
+          );
 
-//         return !isNil(reconciliation);
-//       })
-//       .test(
-//         'validate-open',
-//         'Reconciliation is already completed',
-//         async function(value) {
-//           const reconciliation = await context.dbal.reconciliation.fetchById(
-//             value,
-//           );
+          return !isNil(reconciliation);
+        },
+      )
+      .test(
+        'validate-open',
+        'Reconciliation is already completed',
+        async function validateReconcilationOpen(value) {
+          const reconciliation = await context.dbal.reconciliation.fetchById(
+            value,
+          );
 
-//           return (
-//             isNil(reconciliation) ||
-//             reconciliation.status.toLowerCase() === 'open'
-//           );
-//         },
-//       ),
-//   });
-// }
+          return (
+            isNil(reconciliation) ||
+            reconciliation.status.toLowerCase() === 'open'
+          );
+        },
+      ),
+  });
+}
 
-// export const finishReconciliation = {
-//   validationSchema: validateFinishReconciliation,
-//   resolve: async function finishReconciliation(root, { id }, context) {
-//     await context.requireAuthorization();
-//     return context.dbal.reconciliation
-//       .update(id, { status: 'Complete' })
-//       .then(reconciliation => ({ reconciliation }));
-//   },
-// };
+const finishReconciliation = {
+  validationSchema: validateFinishReconciliation,
+  resolve: async function finishReconciliation(root, { id }, context) {
+    await context.requireAuthorization();
+    return context.dbal.reconciliation
+      .update(id, { status: 'Complete' })
+      .then(reconciliation => ({ reconciliation }));
+  },
+};
 
-module.exports = () => null;
+module.exports = finishReconciliation;
